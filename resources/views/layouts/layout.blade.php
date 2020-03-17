91fa6aaa827e0effebffdbcf2b5c8cff0f8754bc
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-03-17 15:52:46 +0100

    commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,13 @@
+ @include('layouts.partials._header')
+  <body>
+    <div class="container">
+      <header>
+
+      </header>
+      <main>
+pippo
+      </main>
+      <footer>
+
+      </footer>
+ @include('layouts.partials._footer')
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'ViewController@index')->name('view_controller.home');
+Route::get('/corso', 'ViewController@corso')->name('view_controller.corso');
+Route::get('/dopo-corso', 'ViewController@dopoCorso')->name('view_controller.dopoCorso');
+
+
+// studenti
+Route::get('/students', 'StudentController@index')->name('student.home');
+Route::get('/students/show/{id}', 'StudentController@show')->name('student.show');
