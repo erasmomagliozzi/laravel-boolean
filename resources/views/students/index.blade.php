02cffea70eb8a99d7e76ad9d08a2869bb173a568
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-03-17 21:43:07 +0100

    commit

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.layout')
 @section('main')
   @foreach (config('students.students') as $student)
-    {{$student['nome']}}
+  <div class="student">
+    <img src="{{$student['img']}}" alt="{{$student['nome']}}">
+    <h1>{{$student['nome']}} ( {{$student['eta']}} anni)</h1>
+    <h3>Assunt{{ ($student ['genere'] == 'm') ? 'o' : 'a'}} da {{$student ['azienda']}} come {{$student['ruolo']}}</h3>
+    <p>{{$student ['descrizione']}}</p>
+
+  </div>
   @endforeach
 @endsection
